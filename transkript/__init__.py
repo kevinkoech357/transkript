e9f374a9262195a96b6e7a7745ecceb8ecaabf8d
Kevin_Koech <kevinkoech@proton.me>
2023-11-30 22:45:13 +0300

    configure the app using cors, config file and add strict slashes to false

--- transkript/__init__.py
+++ transkript/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_bootstrap import Bootstrap5
+from transkript.config import App_Config
+
 
 bootstrap = Bootstrap5()
 
 def create_app():
     app = Flask(__name__)
     bootstrap.init_app(app)
+
+    # Load configuration from App_Config
+    app.config.from_object(App_Config)
+
+    # Allow URLs with or without trailing slashes
+    app.url_map.strict_slashes = False
+
+    # Initialize CORS
+    CORS(app)
     
     from transkript.views import user
 
