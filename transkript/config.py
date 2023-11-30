8c86321c2556ce63aa68b215feb35a42bae3e223
Kevin_Koech <kevinkoech@proton.me>
2023-11-30 22:43:26 +0300

    add app configuration

---
+++ transkript/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+
+class App_Config:
+    # Secret key for signing cookies
+    SECRET_KEY = os.environ.get("SECRET_KEY", "hello_world!")
+    # Video uploads folder
+    UPLOAD_FOLDER = "uploads"
