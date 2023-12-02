dbea3e21b7db0ebe85622354b97fd5edf4077597
Kevin_Koech <kevinkoech@proton.me>
2023-12-02 23:19:23 +0300

    complete file upload route

--- transkript/views.py
+++ transkript/views.py
@@ -1,39 +1,59 @@
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, request, jsonify
 from werkzeug.utils import secure_filename
 import os
 
-user = Blueprint('user', __name__)
+user = Blueprint("user", __name__)
 
-ALLOWED_EXTENSIONS = {'mpeg', 'mp3', 'mpeg3', 'mpga', 'mp4', 'm4a', 'wav', 'webm', 'mp4'}
+ALLOWED_EXTENSIONS = {
+    "mpeg",
+    "mp3",
+    "mpeg3",
+    "mpga",
+    "mp4",
+    "m4a",
+    "wav",
+    "webm",
+    "mp4",
+}
+
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@user.route('/', methods=['GET'])
+
+@user.route("/", methods=["GET"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return redirect(request.url)
 
-    file = request.files['file']
+@user.route("/upload", methods=["POST"])
+def upload():
+    try:
+        if "file" not in request.files:
+            print("No file part")
+            return jsonify({"error": "No file part"})
 
-    if file.filename == '':
-        return redirect(request.url)
+        file = request.files["file"]
 
-    if file and allowed_file(file.filename):
-        # Use secure_filename to ensure a safe filename
-        filename = secure_filename(file.filename)
+        if file.filename == "":
+            print("No selected file")
+            return jsonify({"error": "No selected file"})
 
-        # Save the file to the upload folder
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
+        if file and allowed_file(file.filename):
+            # Save the file to the upload folder
+            file_path = os.path.join(
+                current_app.config["UPLOAD_FOLDER"], secure_filename(file.filename)
+            )
+            file.save(file_path)
 
-        # TO BE CONTINUED
+            # Transcription
 
-        return 'File uploaded successfully'
+            print("File uploaded successfully")
+            return jsonify({"success": "File uploaded successfully"})
 
-    return 'Invalid file type'
+        print("Invalid file type")
+        return jsonify({"error": "Invalid file type"})
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return jsonify({"error": "An error occurred during file upload"})
