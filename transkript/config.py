d7f868f22840135b63ff38282a5eee838508817c
Kevin_Koech <kevinkoech@proton.me>
2023-12-05 17:45:02 +0300

    add link to screenrekod and source code

--- transkript/config.py
+++ transkript/config.py
@@ -8,4 +8,4 @@ class App_Config:
     # Secret key for signing cookies
     SECRET_KEY = os.environ.get("SECRET_KEY", "hello_world!")
     # Video uploads folder
-    UPLOAD_FOLDER = "uploads"
+    UPLOAD_FOLDER = "/home/ubuntu/transkript/uploads"
