558cbbc8abb23b1448cfb39063c757e16c38ab0d
Kevin_Koech <kevinkoech@proton.me>
2023-12-16 20:02:04 +0300

    access celery instance from ext_celery

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from transkript import create_app
+from transkript import create_app, ext_celery
 
 app = create_app()
+celery = ext_celery.celery
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
