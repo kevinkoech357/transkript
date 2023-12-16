7e52e550e8ebd040c8fbe4a3e4e2b28388155a18
Kevin_Koech <kevinkoech@proton.me>
2023-12-16 20:00:03 +0300

    add celery configuration

--- transkript/config.py
+++ transkript/config.py
@@ -9,3 +9,9 @@ class App_Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "hello_world!")
     # Video uploads folder
     UPLOAD_FOLDER = "/home/ubuntu/transkript/uploads"
+    # Celery configuration
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
+    CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+    CELERY_TASK_IGNORE_RESULT = True
+    # Log file path
+    LOG_FILE_PATH = "transkript.log"
